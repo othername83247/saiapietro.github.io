ef873bb864fc13843814b90be553c6f2c2d7256e
saiapietro <35384755+saiapietro@users.noreply.github.com>
2018-03-03 14:31:51 -0500

    sprint 6

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function arraySearch(arr,val) {
+    for (var i=0; i<arr.length; i++)
+        if (arr[i] === val)                    
+            return i;
+    return false;
+}
 $( document).ready(function() {
 
 $( ".cross" ).hide();
@@ -17,7 +23,24 @@ $( ".hamburger" ).show();
 });
 });
 
-$( "#place1").click(function(){
-	$("#place1info").slideToggle("slow");
+$( ".place").click(function(){
+	if (typeof x != 'undefined'){
+		
+		if (x == "#place"  +String(arraySearch(document.querySelectorAll('.place'),this)) + "info"){
+		$(x).slideToggle("slow", function(){
+		$(x).hide();
+		$(x).remove();});}}
+	/*else{*/
+	x = '#place' + String(arraySearch(document.querySelectorAll('.place'),this)) + 'info'
+	$(this).after("<div id = 'place" + String(arraySearch(document.querySelectorAll('.place'),this)) + "info' style = 'display: none;'> <img src='http://onelovemassive.com/wp-content/uploads/2016/10/test-image.png' height = '200' width = '400' class = 'centered'> <p class = 'centered'>Test description</p> </div>")
+	/*}*/
+	
+	$(x).slideToggle("slow", function(){
+		
+		$(x)['0'].style.display = 'block';
+	});
+	
+	});
+	
 });
-});
+
